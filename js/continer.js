1df42b759cb925f25d344e60e4d13105ff67e140
Kayling2506 <98617301+Kayling2506@users.noreply.github.com>
2025-03-17 01:50:56 -0600

    Update continer.js

--- js/continer.js
+++ js/continer.js
@@ -3,3 +3,7 @@ const mobileMenu = document.getElementById('mobile-menu');
 menuToggle.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 });
+
+app.get('../index.hml', (req, res) => {
+    res.sendFile(__dirname + '/html/index.html');
+});
